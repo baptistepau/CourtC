db1b3bc00993f5aafcaff2d2b763a71ba4fb9996
Baptiste Pauchet <63608854+SpeedyCreator@users.noreply.github.com>
2023-04-03 18:27:16 +0200

    Backup code

--- Ccourse2/chronometre/Ccourse.h
+++ Ccourse2/chronometre/Ccourse.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "CChrono.h"
+#include "CParticipant.h"
 class Ccourse
 {
+private:
+	CChrono leChrono;
+	CParticipant lesParticipant[10];
+public :
+	Ccourse();
+	~Ccourse();
+	bool Demarrer();
+	bool Arreter();
+	bool lireTempsCourse(char* temps, int taille);
+	bool lireMeilleurTemps(int participant, char* temps, int taille);
+	bool lireDerniersTempsTours(int participant, char* temps, int taille);
+	int lireNbTours(int participant);
 };
 
